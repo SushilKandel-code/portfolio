8a9a42b93129ae1b4387fdf5b01720d4569ab1b0
Sushil Kandel <sushil.kandel16@gmail.com>
2025-09-04 09:34:27 +1000

    adding to new branch

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
                 <div className='content'>
                     <br></br>
                     <br></br>
-                   <p style={{fontSize: '20px', textAlign: 'justify', padding: '20px'}}>A passionate Developer who loves turning ideas into reality through clean code, user‑friendly interfaces, and scalable solutions.<br></br>
+                   <p style={{fontSize: '20px', textAlign: 'justify', padding: '30px'}}>A passionate Developer who loves turning ideas into reality through clean code, user‑friendly interfaces, and scalable solutions.<br></br>
                      I specialize in building modern, responsive web applications using JavaScript, React, and Node.js. My mission is to create software that not only works, but also feels intuitive and enjoyable to use. </p>
 
                 </div>
